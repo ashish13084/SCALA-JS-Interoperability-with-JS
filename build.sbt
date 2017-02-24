4bbb3005a596584cfd7e0699fd8c631c45172136
Ashish Kumar <ashishkumar@in-ashishkumar.local>
2017-02-24 11:02:16 +0530

    Call to JS from Scala-JS

--- build.sbt
+++ build.sbt
@@ -10,3 +10,4 @@ libraryDependencies ++= Seq(
   "org.scala-js" %%% "scalajs-dom" % "0.9.1",
   "com.lihaoyi" %%% "scalatags" % "0.6.1"
 )
+jsDependencies += ProvidedJS / "bar.js"
--- src/main/scala/example/ScalaJSExample.scala
+++ src/main/scala/example/ScalaJSExample.scala
@@ -1,44 +1,42 @@
 package example
-import scala.scalajs.js.annotation.JSExport
-import org.scalajs.dom
-import org.scalajs.dom.html
-import scala.util.Random
+import scala.scalajs.js.annotation.{JSGlobalScope, JSName}
+import scala.scalajs.js
+import scala.scalajs.js.JSApp
+/*
+@js.native
+@JSName("myFun")
+object MyFun extends js.Object {
+  val name:String=js.native
+  def square(x:Int):Int = js.native
+}
+*/
 
-case class Point(x: Int, y: Int){
-  def +(p: Point) = Point(x + p.x, y + p.y)
-  def /(d: Int) = Point(x / d, y / d)
+@js.native
+@JSGlobalScope
+object DOMGlobalScope extends js.Object {
+  def square(x:Int):Int=js.native
 }
 
-@JSExport
-object ScalaJSExample {
-  @JSExport
-  def main(canvas: html.Canvas): Unit = {
-    val ctx = canvas.getContext("2d")
-                    .asInstanceOf[dom.CanvasRenderingContext2D]
+@js.native
+@JSName("Apple")
+class apple(t:String) extends js.Object {
+  val types:String=js.native
+  val color:String=js.native
+  def getInfo():String = js.native
+}
 
-    var count = 0
-    var p = Point(0, 0)
-    val corners = Seq(Point(255, 255), Point(0, 255), Point(128, 0))
-
-    def clear() = {
-      ctx.fillStyle = "black"
-      ctx.fillRect(0, 0, 255, 255)
-    }
-
-    def run = for (i <- 0 until 10){
-      if (count % 3000 == 0) clear()
-      count += 1
-      p = (p + corners(Random.nextInt(3))) / 2
-
-      val height = 512.0 / (255 + p.y)
-      val r = (p.x * height).toInt
-      val g = ((255-p.x) * height).toInt
-      val b = p.y
-      ctx.fillStyle = s"rgb($g, $r, $b)"
-
-      ctx.fillRect(p.x, p.y, 1, 1)
-    }
-
-    dom.window.setInterval(() => run, 50)
+object ScalaJSExample extends JSApp{
+  def main(): Unit ={
+    //Simple function
+   /* println( MyFun.name )
+     println( MyFun.square(4) )
+*/
+    println( DOMGlobalScope.square(4) )
+    //apple class
+    val apple_obj=new apple("fruit")
+    println(apple_obj.color)
+    println(apple_obj.getInfo())
   }
 }
+
+
